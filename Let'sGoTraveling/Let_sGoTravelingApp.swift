9e1dd5268c028162d5eaec3017d5f1aa8d3d31b2
choijaegwon <chl9338@naver.com>
2023-08-20 11:05:00 +0900

    Initial Commit

---
+++ Let'sGoTraveling/Let_sGoTravelingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Let_sGoTravelingApp.swift
+//  Let'sGoTraveling
+//
+//  Created by Jae kwon Choi on 2023/08/20.
+//
+
+import SwiftUI
+
+@main
+struct Let_sGoTravelingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
