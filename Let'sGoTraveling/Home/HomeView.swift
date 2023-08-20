3b372be686c6dc2c19d06aea1c0aaf1073dc78ed
choijaegwon <chl9338@naver.com>
2023-08-20 12:52:17 +0900

    feat: TopTabBar 구현

---
+++ Let'sGoTraveling/Home/HomeView.swift
@@ -0,0 +1,37 @@
+//
+//  HomeView.swift
+//  Let'sGoTraveling
+//
+//  Created by Jae kwon Choi on 2023/08/20.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+
+    @State var currentTab: Int = 0
+
+    var body: some View {
+        NavigationStack {
+            ZStack(alignment: .top) {
+                TabView(selection: self.$currentTab) {
+
+                    Text("test0")
+                        .tag(0)
+
+                    Text("test1")
+                        .tag(1)
+                }
+                .padding(.top, 50)
+                TopTabBarView(currentTab: self.$currentTab)
+            }
+        }
+    }
+}
+
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
---
+++ Let'sGoTraveling/Home/TopTabBarView.swift
@@ -0,0 +1,86 @@
+//
+//  TopTabBarView.swift
+//  Let'sGoTraveling
+//
+//  Created by Jae kwon Choi on 2023/08/20.
+//
+
+import SwiftUI
+
+struct TopTabBarView: View {
+
+    @Binding var currentTab: Int
+    @Namespace var namespace
+
+    var TopTabBarOptions: [String] = ["랜덤", "팔로잉"]
+
+    private enum Constants {
+        /// 아이콘 width
+        static let viewWidth: CGFloat = 150.0
+
+        /// 아이콘 height
+        static let viewHeight: CGFloat = 50.0
+    }
+
+    var body: some View {
+        HStack {
+            HStack {
+                ForEach(self.TopTabBarOptions.indices, id: \.self) { index in
+                    TopTabBarItem(
+                        currentTab: self.$currentTab, namespace: self.namespace.self,
+                        topTabBarItemName: self.TopTabBarOptions[index],
+                        tab: index
+                    )
+                }
+            }
+            .frame(width: Constants.viewWidth)
+            Spacer()
+        }
+        .background(Color.white)
+        .frame(height: Constants.viewHeight)
+    }
+}
+
+struct TopTabBarItem: View {
+
+    @Binding var currentTab: Int
+
+    let namespace: Namespace.ID
+    var topTabBarItemName: String
+    var tab: Int
+
+    private enum Constants {
+        /// 아이콘 Size
+        static let iconSize: CGFloat = 18.0
+
+        /// 아이콘 height
+        static let barHeight: CGFloat = 4.0
+    }
+
+    var body: some View {
+        Button {
+            self.currentTab = self.tab
+        } label: {
+            VStack {
+                Spacer()
+                Text(self.topTabBarItemName)
+                    .foregroundColor(.black)
+                    .font(.system(size: Constants.iconSize, weight: .medium))
+                if self.currentTab == self.tab {
+                    Color.black
+                        .frame(height: Constants.barHeight)
+                        .matchedGeometryEffect(id: "underLine", in: self.namespace, properties: .frame)
+                } else {
+                    Color.clear.frame(height: Constants.barHeight)
+                }
+            }
+            .animation(.spring(), value: self.currentTab)
+        }
+    }
+}
+
+struct TopTabBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        TopTabBarView(currentTab: .constant(0))
+    }
+}
